7ccd44c57195305acb8cb58101adbda56b1158ac
Will Johnson <trashmoldwilliams@gmail.com>
2016-03-04 08:19:43 -0800

    Add basic tests for Store class.

--- Tests/StoreTest.cs
+++ Tests/StoreTest.cs
@@ -12,5 +12,64 @@ namespace ShoeStores.Objects
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=shoe_stores_test;Integrated Security=SSPI;";
     }
+
+    [Fact]
+    public void Test_StoresEmptyAtFirst()
+    {
+      int result = Store.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_EqualOverride_TrueForSameName()
+    {
+      var storeOne = new Store("Target");
+      var storeTwo = new Store("Target");
+
+      Assert.Equal(storeOne, storeTwo);
+    }
+
+    [Fact]
+    public void Test_Save_SavesStoreDataBase()
+    {
+      var testStore = new Store("Target");
+      testStore.Save();
+
+      var testList = new List<Store>{testStore};
+      var result = Store.GetAll();
+
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_SaveAssignsIdToOBjects()
+    {
+      var testStore = new Store("Target");
+      testStore.Save();
+
+      var savedStore = Store.GetAll()[0];
+
+      int result = savedStore.GetId();
+      int testId = testStore.GetId();
+
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_FindStoreInDataBase()
+    {
+      var testStore = new Store("Target");
+      testStore.Save();
+
+      var foundStore = Store.Find(testStore.GetId());
+
+      Assert.Equal(testStore, foundStore);
+    }
+
+    public void Dispose()
+    {
+      Store.DeleteAll();
+    }
   }
 }
